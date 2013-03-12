5dd58110830a3e53da34c907ca211b21f6f70bff
AndrePinto-1090534 <1090534@isep.ipp.pt>
2013-03-12 15:31:39 +0000

    criação do projecto

---
+++ PL5java/src/pl5java/PL5java.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl5java;
+
+/**
+ *
+ * @author André Pinto
+ */
+public class PL5java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
